645b2825c26d0c63720204d95a2d2329588d52ae
Minh Ngo <mh98ngo@gmail.com>
2021-03-15 03:30:25 +0100

    Add plugins

--- gridsome.config.js
+++ gridsome.config.js
@@ -5,6 +5,38 @@
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
 module.exports = {
-  siteName: 'Gridsome',
-  plugins: []
-}
+  siteName: "Gridsome Portfolio Starter",
+  siteDescription:
+    "A simple portfolio theme for Gridsome powered by Tailwind CSS v1",
+  siteUrl: "https://gridsome-portfolio-starter.netlify.com",
+  plugins: [
+    {
+      use: "gridsome-plugin-tailwindcss",
+      options: {
+        tailwindConfig: "./tailwind.config.js",
+        presetEnvConfig: {},
+        shouldImport: false,
+        shouldTimeTravel: false,
+      },
+    },
+    {
+      use: "@gridsome/source-filesystem",
+      options: {
+        path: "blog/**/*.md",
+        typeName: "Post",
+        refs: {
+          tags: {
+            typeName: "Tag",
+            create: true,
+          },
+        },
+      },
+    },
+    {
+      use: "@gridsome/plugin-sitemap",
+      options: {
+        cacheTime: 600000, // default
+      },
+    },
+  ],
+};
--- tailwind.config.js
+++ tailwind.config.js
@@ -41,7 +41,6 @@ module.exports = {
         primary: "var(--bg-background-primary)",
         secondary: "var(--bg-background-secondary)",
         tertiary: "var(--bg-background-tertiary)",
-
         form: "var(--bg-background-form)",
       },
 
